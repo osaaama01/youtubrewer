c182256fb966c9e3999a4423e91d0a0cdd9fbd5c
Muhammad Uzair Arshad <uzair.arshad@systemsltd.com>
2022-05-08 18:45:51 +0500

    Refining grid system to be responsive on different screens and adding full banner at bottom

--- src/pages/home/home.component.jsx
+++ src/pages/home/home.component.jsx
@@ -1,44 +1,49 @@
 import React from "react";
 
 import { Grid } from "@mui/material";
-import coffee from "../../assets/coffee.PNG"
+import banner from "../../assets/banner.PNG"
+import full_banner from "../../assets/full_banner.PNG"
 
 import './home.styles.scss';
-import { Circle } from "../../components/Circle";
 
-
-const Home=(p)=>(
+const Home=()=>(
 
     <div className="container">
 
-        <Grid container >
-            <Grid className="first_column" item xs={3}  >
-                <div className="coffee_row">
-                    <img className="coffee_image" src={coffee} alt='Coffee'/>
+        <Grid container spacing={2}>
+            <Grid className="first_column" item sm={4} md={3} lg={3}   display={{xs: "none",sm:"block",md:"block",lg:"block"}} >   {/* 33.33% width on desktop and 50% width on mobile */}
+                <div className="banner_row">
+                    <img className="banner_image" src={banner} alt="banner.png"/>
                 </div>
-                <div className="coffee_row">
-                    <img className="coffee_image" src={coffee} alt='Coffee'/>
+                <div className="banner_row">
+                    <img className="banner_image" src={banner} alt="banner.png" />
                 </div>
-                <div>
-                    <img className="coffee_image" src={coffee} alt='Coffee'/>
+                <div className="last_banner">
+                    <img className="banner_image" src={banner} alt="banner.png" />
                 </div>
             </Grid>
-            <Grid className="second_column" item xs={6}>
-                <Circle size={'20px'} backgroundColor={'#04CF55'} borderColor={'#000000'}/>
+            <Grid className="second_column" item xs={12} sm={4} md={6} lg={6}  >  {/* 50% width on desktop and 66.66% width on mobile */}
+                I am middle component
             </Grid>
-            <Grid item xs={3}>
-                <div className="coffee_row">
-                    <img className="coffee_image" src={coffee} alt='Coffee'/>
+              <Grid item sm={4} md={3} lg={3}  display={{xs: "none",sm:"block",md:"block",lg:"block"}} >       
+        
+                <div className="banner_row">
+                    <img className="banner_image" src={banner} alt="banner.png" />
                 </div>
-                <div className="coffee_row">
-                    <img className="coffee_image" src={coffee} alt='Coffee'/>
+                <div className="banner_row">
+                    <img className="banner_image" src={banner} alt="banner.png" />
                 </div>
-                <div>
-                    <img className="coffee_image" src={coffee} alt='Coffee'/>
+                <div className="last_banner">
+                    <img className="banner_image" src={banner} alt="banner.png" />
                 </div>
             </Grid>
         
         </Grid>
+
+        <div >
+            <img className="full_banner_image" src={full_banner} alt="banner.png" />
+        </div>
+
     </div>
 )
 
